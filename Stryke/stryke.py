4dbc39472b1ff23ed643b44084bf95293f5fc6fd
Kevin P Nebiolo, PhD. <61742537+knebiolo@users.noreply.github.com>
2024-11-07 12:05:02 -0500

    Update stryke.py

--- Stryke/stryke.py
+++ Stryke/stryke.py
@@ -41,7 +41,7 @@ warnings.filterwarnings("ignore")
 from scipy.stats import beta
 import xlrd
 import networkx as nx
-from Stryke.hydrofunctions import hydrofunctions as hf
+from stryke.hydrofunctions import hydrofunctions as hf
 import requests
 #import geopandas as gp
 import statsmodels.api as sm
