16b533a173d5084c0e8b188b4a56b6fdbe0f9b86
Kevin P Nebiolo, PhD. <61742537+knebiolo@users.noreply.github.com>
2025-03-03 19:25:45 -0500

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -578,4 +578,4 @@ def clear_folder(folder_path):
                 print(f"Failed to delete {file_path}: {e}")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
