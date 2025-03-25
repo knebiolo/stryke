f324eda350ca92c19f98a1ab3f8cc2f961a0c188
Kevin P Nebiolo, PhD. <61742537+knebiolo@users.noreply.github.com>
2025-03-25 00:23:43 -0400

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -2042,16 +2042,18 @@ def population():
             "length scale": "Length Scale"
         }
         
-        proj_dir = session.get("proj_dir", "/tmp")  # Or your configured project directory
-        pop_csv_path = os.path.join(proj_dir, "population_params.csv")
-        df_population_summary.to_csv(pop_csv_path, index=False)
-        session['population_csv_path'] = pop_csv_path
-        print("Saved population parameters to file:", pop_csv_path, flush=True)
 
+        
+ 
         
         df_population_summary = df_population.rename(columns=summary_column_mapping)
         session['population_dataframe_for_summary'] = df_population_summary.to_json(orient='records')
-        
+        proj_dir = session.get("proj_dir", "/tmp")  # Or your configured project directory
+        pop_csv_path = os.path.join(proj_dir, "population_params.csv")
+        df_population_summary.to_csv(pop_csv_path, index=False)   
+        session['population_csv_path'] = pop_csv_path
+        print("Saved population parameters to file:", pop_csv_path, flush=True)
+
         print("Population DataFrame for summary:", session.get('population_dataframe_for_summary'), flush=True)
         
         print("Population parameters saved successfully! Redirecting...", flush=True)
